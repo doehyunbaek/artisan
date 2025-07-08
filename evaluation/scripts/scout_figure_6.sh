24d3250ac73ab0aafab8d0050bc0bfbd118d4dd2
Doehyun Baek <doehyunbaek@gmail.com>
2025-07-08 09:24:36 +0000

    add prompts

--- evaluation/scripts/scout_figure_6.sh
+++ evaluation/scripts/scout_figure_6.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
-docker run --rm --mount "type=bind,source=.,target=/analysis-output" artisan25/scout
+set -e
+docker rm -f scout 2>/dev/null || true
+docker run -d --name scout artisan25/scout sleep infinity
+
+docker exec -it scout R -e "rmarkdown::render('/analysis-source/scout_results.Rmd', output_file='/analysis-output/scout_results.pdf')"
--- scripts/scout_figure_6.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-docker rm -f scout 2>/dev/null || true
-docker run -d --name scout artisan25/scout sleep infinity
-
-docker exec -it scout R -e "rmarkdown::render('/analysis-source/scout_results.Rmd', output_file='/analysis-output/scout_results.pdf')"
